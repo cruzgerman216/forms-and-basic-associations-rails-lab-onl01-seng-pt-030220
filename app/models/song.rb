f0e0343ff44a73034ea065d11c38cc1b55698f06
German Cruz <cruzgerman216@yahoo.com>
2020-08-04 02:45:54 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -23,11 +23,9 @@ class Song < ActiveRecord::Base
   end
 
   def note_ids=(ids)
-    ids.each do |content|
-      unless content.empty?
-        self.notes << Note.create(content: content)
-        self.save
-      end
+    ids.each do |id|
+      note = Note.find(id)
+      self.notes << note
     end
   end
 end
