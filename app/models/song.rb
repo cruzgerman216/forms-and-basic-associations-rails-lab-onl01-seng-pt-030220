2a951dad10535ca94a5675d67eb0b9e12997509d
German Cruz <cruzgerman216@yahoo.com>
2020-08-04 01:34:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,9 +5,9 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name:name)
   end
-  # def artist_name
-  #   self.artist ? self.artist.name : nil
-  # end
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
   def song_genre_id=(id)
     self.genre.id = id.to_i
   end
