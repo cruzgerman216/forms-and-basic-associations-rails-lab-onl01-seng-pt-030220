faf6275ec2ba565f2de2161ee4b4c71303285a8a
German Cruz <cruzgerman216@yahoo.com>
2020-08-04 00:44:44 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-  def song_artist_name=(name)
+  def artist_name=(name)
     self.artist = Artist.find_or_create_by(name:name)
   end
-  def song_artist_name
+  def artist_name
     self.artist ? self.artist.name : nil
   end
 end
